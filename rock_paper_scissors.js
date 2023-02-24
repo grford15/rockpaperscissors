481a841751cb89ae28740e8242aefa2cdc9248ae
Greg <g.rutherford10@gmail.com>
2023-02-24 15:43:50 +0000

    add basic function to pick computers answer

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,1 +1,11 @@
 console.log("This is working");
+
+function getComputerChoice() {
+	let answers = ["rock", "paper", "scissors"];
+
+	let choice = Math.floor(Math.random() * answers.length);
+
+	return answers[choice];
+}
+
+console.log(getComputerChoice());
