8bdaeed06fce508a22fed99a5662cf276eb9b369
Greg <g.rutherford10@gmail.com>
2023-02-25 15:59:50 +0000

    create a play round function which plays a round of the game

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -12,6 +12,30 @@ let computerAnswer = getComputerChoice();
 
 console.log(computerAnswer);
 
-let playerAnswer = prompt("Pick either Rock, Paper or Scissors");
+let playerAnswer = prompt("Pick either Rock, Paper or Scissors").toLowerCase();
 
-console.log(playerAnswer);
+function playRound(computerAnswer, playerAnswer) {
+	if (computerAnswer === "rock" && playerAnswer === "rock") {
+		console.log("It's a draw!");
+	} else if (computerAnswer === "rock" && playerAnswer === "paper") {
+		console.log("You Win!");
+	} else if (computerAnswer === "rock" && playerAnswer === "scissors") {
+		console.log("You Lose!");
+	} else if (computerAnswer === "paper" && playerAnswer === "rock") {
+		console.log("You Lose!");
+	} else if (computerAnswer === "paper" && playerAnswer === "paper") {
+		console.log("It's a draw!");
+	} else if (computerAnswer === "paper" && playerAnswer === "scissors") {
+		console.log("You Win!");
+	} else if (computerAnswer === "scissors" && playerAnswer === "rock") {
+		console.log("You Win!");
+	} else if (computerAnswer === "scissors" && playerAnswer === "paper") {
+		console.log("You Lose!");
+	} else if (computerAnswer === "scissors" && playerAnswer === "scissors") {
+		console.log("It's a Draw!");
+	} else {
+		console.log("You haven't entered a suitable answer");
+	}
+}
+
+playRound(computerAnswer, playerAnswer);
